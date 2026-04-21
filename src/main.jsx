45f7b4c5c3b7cba8f007d23d022dfb01568b8fbb
Michelle Sullivan <shellyryan18@gmail.com>
2026-04-21 09:23:36 -0500

    fix intake route on vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import IntakeForm from "./IntakeForm.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/intake" element={<IntakeForm />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/intake" element={<IntakeForm />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
